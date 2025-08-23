ef714e978caa6b1357f8753d013215a4bb73be7d
Zamelane <zamelane@vk.com>
2025-08-23 15:27:59 +0700

    Главная страница (авторизация + редирект)

--- .section.php
+++ .section.php
@@ -1,9 +1,9 @@
 <?
 $sSectionName = 'Главная';
 $arDirProperties = array(
-	'title' => 'Title',
-	'description' => 'Description',
-	'keywords' => 'Keywords',
+	'title' => 'Бонусник',
+	'description' => 'Тестовое задание от sibcode',
+	'keywords' => 'тествое, задание, test, task, sibcode',
 	'robots' => 'index, follow'
 );
 ?>
--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
-$APPLICATION->SetTitle('Главная');
-?> 
+define("NEED_AUTH", true);
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
-<?
-require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+if (isset($_REQUEST["backurl"]) && strlen($_REQUEST["backurl"])>0)
+    LocalRedirect($backurl);
+
+$APPLICATION->SetTitle("Авторизация");
 ?>
+
+<?php
+// Админа не редиректим, вдруг страницу редактировать надо ...?
+if ($USER->IsAuthorized() && !$USER->IsAdmin()) {
+    LocalRedirect('/profile/');
+    exit();
+}
+?>
+
+<p>Вы зарегистрированы и успешно авторизовались.</p>
+<p>Используйте административную панель в верхней части экрана для быстрого доступа к функциям управления структурой и информационным наполнением сайта. Набор кнопок верхней панели отличается для различных разделов сайта. Так отдельные наборы действий предусмотрены для управления статическим содержимым страниц, динамическими публикациями (новостями, каталогом, фотогалереей) и т.п.</p>
+<p><a href="<?=SITE_DIR?>profile">Перейти в свой профиль</a></p>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
